96d8628d9de4564c8b9bbcff18bd76c376cc972e
saranyuavh <saranyu38@gmail.com>
2021-05-16 23:46:31 +0530

    typography added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,12 +3,26 @@ import './Header.css' ;
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+const customStyles = {
+    content: {
+        top :'50%',
+        left: '50%',
+        right: 'auto',
+        bottom : 'auto',
+        marginRight : '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+}
 
 class Header extends Component {
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value:0
         }
     }
 
@@ -20,6 +34,15 @@ class Header extends Component {
         this.setState({ modalIsOpen: false });
     }
 
+    tabChangeHandler = (event, value) => {
+        this.setState({ value });
+    }
+
+    const TabContainer = function() {
+        return(
+            <Typography component="div" style={{padding:0}}></Typography>
+        );
+    }
     render() {
         return (
             <div>
@@ -33,8 +56,16 @@ class Header extends Component {
                     ariaHideApp={false}
                     isOpen={this.state.modalIsOpen}
                     contentLabel="Login"
-                    onRequestClose={this.closeModalHandler}>
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}
+                >
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
+                    <TabContainer>
 
+                    </TabContainer>
                 </Modal>
             </div>
         )
